b2dde51581976916f839f5aa4f82262d8a209a0f
Matej Koncal <matej.koncal@gmail.com>
2020-11-13 23:02:37 +0100

    add anonymous error handler problem

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
 import express from 'express';
 
 export const app = express();
+
+app.use((req, res, next) => {
+
+});
+
+app.use([(req, res, next) => {
+
+}]);
+
+app.use((err, req, res, next) => {
+
+});
+
+app.use([(err, req, res, next) => {
+
+}]);
