79d030a85420432bcc5c89d475e0c1a0c3889e19
Matej Koncal <matej.koncal@gmail.com>
2020-11-23 18:41:01 +0100

    add port normalization

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,10 +1,22 @@
 import 'source-map-support/register';
 import { app } from '../app';
 import http from 'http';
+import { getLogger } from '../utils';
 
-// Get port from environment and store in Express.
-// TODO: Normalize port
-const SERVER_PORT = process.env.PORT || '8000';
+const LOGGER = getLogger();
+
+function normalizePort(val: string = '8000') {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port) || port <= 0) {
+    LOGGER.warn('PORT must be number greater then zero. Replaced with 8000');
+    return 8000;
+  }
+  return port;
+}
+
+// Get port from environment and store it in Express
+const SERVER_PORT = normalizePort(process.env.PORT);
 app.set('port', SERVER_PORT);
 
 // Create HTTP server.
@@ -13,5 +25,5 @@ const server = http.createServer(app);
 // TODO: Add on error action
 
 server.listen(SERVER_PORT, () => {
-  console.log(`Listening on port ${SERVER_PORT}`);
+  LOGGER.info(`Listening on port ${SERVER_PORT}`);
 });
