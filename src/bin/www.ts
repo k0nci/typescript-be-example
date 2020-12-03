fe8deb646070177659fcaef78bbd27a6666c1ebd
Matej Koncal <matej.koncal@gmail.com>
2020-12-03 22:10:29 +0100

    add environment var config

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,6 +1,8 @@
 import 'source-map-support/register';
-import { app } from '../app';
+import 'dotenv/config';
+import config from 'config';
 import http from 'http';
+import { app } from '../app';
 import { getLogger } from '../utils';
 
 const LOGGER = getLogger();
@@ -9,14 +11,18 @@ function normalizePort(val: string = '8000') {
   const port = parseInt(val, 10);
 
   if (isNaN(port) || port <= 0) {
-    LOGGER.warn('PORT must be number greater then zero. Replaced with 8000');
+    LOGGER.warn('SERVER_PORT must be number greater then zero. WARNING: Replaced with 8000');
     return 8000;
   }
   return port;
 }
 
+if (!process.env['NODE_CONFIG_DIR']) {
+  process.env['NODE_CONFIG_DIR'] = `${process.cwd()}/config`;
+}
+
 // Get port from environment and store it in Express
-const SERVER_PORT = normalizePort(process.env.PORT);
+const SERVER_PORT = normalizePort(config.get('service.port'));
 app.set('port', SERVER_PORT);
 
 // Create HTTP server.
