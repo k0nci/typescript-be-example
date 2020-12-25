66d8907b176e8fe6d29d7fc81db7a6aeaebe2b6c
Matej Koncal <matej.koncal@gmail.com>
2020-12-25 15:39:04 +0100

    refactor status code naming

--- src/utils/errors/InternalServerError.ts
+++ src/utils/errors/InternalServerError.ts
@@ -1,13 +1,13 @@
 import { HttpError } from './HttpError';
 
 export class InternalServerError extends HttpError {
-  private static STATUS = 500;
+  private static STATUS_CODE = 500;
 
   constructor(message: string | Error = 'Internal server error') {
     if (message instanceof Error) {
-      super('Internal server error', InternalServerError.STATUS, message);
+      super('Internal server error', InternalServerError.STATUS_CODE, message);
     } else {
-      super(message, InternalServerError.STATUS);
+      super(message, InternalServerError.STATUS_CODE);
     }
     Object.setPrototypeOf(this, new.target.prototype);
   }
--- src/utils/errors/ValidationError.ts
+++ src/utils/errors/ValidationError.ts
@@ -1,10 +1,10 @@
 import { HttpError } from './HttpError';
 
 export class ValidationError extends HttpError {
-  private static STATUS = 400;
+  private static STATUS_CODE = 400;
 
   constructor(message: string = 'Validation error') {
-    super(message, ValidationError.STATUS);
+    super(message, ValidationError.STATUS_CODE);
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
